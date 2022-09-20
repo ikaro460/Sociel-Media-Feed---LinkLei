578bef9eda677b9e5f465e1dedb774d076ee3549
Icaro Nascimento <ikaro_460@hotmail.com>
2022-09-20 02:22:57 -0300

    feat: add dropdown menu to post

--- social-media-feed-linklei/src/Components/Post/index.jsx
+++ social-media-feed-linklei/src/Components/Post/index.jsx
@@ -1,15 +1,27 @@
-import { Figure, Image } from "react-bootstrap";
+import Figure from "react-bootstrap/Figure";
+import Image from "react-bootstrap/Image";
+import Dropdown from "react-bootstrap/Dropdown";
 
 export const Post = ({ avatar, authorName, date, category, image, text }) => {
   return (
     <li>
-      <Image
-        roundedCircle={true}
-        width={171}
-        height={180}
-        alt="avatar"
-        src={avatar}
-      />
+      <Dropdown>
+        <Dropdown.Toggle variant="success" id="dropdown-basic" />
+
+        <Dropdown.Menu>
+          <Dropdown.Item href="#/action-1">Editar</Dropdown.Item>
+          <Dropdown.Item href="#/action-2">Excluir</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+      <Figure>
+        <Image
+          roundedCircle
+          width={171}
+          height={180}
+          alt="avatar"
+          src={avatar}
+        />
+      </Figure>
       <h2>{authorName}</h2>
       <h6>
         Publicado em{" "}
