db486d1c94e993f1f0a1660ca8800fbe19600af8
Icaro Nascimento <ikaro_460@hotmail.com>
2022-09-20 02:38:31 -0300

    feat: create styles for Post component

--- social-media-feed-linklei/src/Components/Post/index.jsx
+++ social-media-feed-linklei/src/Components/Post/index.jsx
@@ -1,41 +1,49 @@
 import Figure from "react-bootstrap/Figure";
 import Image from "react-bootstrap/Image";
 import Dropdown from "react-bootstrap/Dropdown";
+import { Container } from "./styles";
 
 export const Post = ({ avatar, authorName, date, category, image, text }) => {
   return (
-    <li>
-      <Dropdown>
-        <Dropdown.Toggle variant="success" id="dropdown-basic" />
+    <Container>
+      <li>
+        <Dropdown>
+          <Dropdown.Toggle variant="success" id="dropdown-basic" />
 
-        <Dropdown.Menu>
-          <Dropdown.Item href="#/action-1">Editar</Dropdown.Item>
-          <Dropdown.Item href="#/action-2">Excluir</Dropdown.Item>
-        </Dropdown.Menu>
-      </Dropdown>
-      <Figure>
-        <Image
-          roundedCircle
-          width={171}
-          height={180}
-          alt="avatar"
-          src={avatar}
-        />
-      </Figure>
-      <h2>{authorName}</h2>
-      <h6>
-        Publicado em{" "}
-        {
-          date //12 de Dezembro de 2012 as 16:00
-        }
-      </h6>
-      <h3>{category}</h3>
-      <p>{text}</p>
-      {image && (
+          <Dropdown.Menu>
+            <Dropdown.Item href="#/action-1">Editar</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">Excluir</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
         <Figure>
-          <Figure.Image width={171} height={180} alt="post-image" src={image} />
+          <Image
+            roundedCircle
+            width={171}
+            height={180}
+            alt="avatar"
+            src={avatar}
+          />
         </Figure>
-      )}
-    </li>
+        <h2>{authorName}</h2>
+        <h6>
+          Publicado em{" "}
+          {
+            date //12 de Dezembro de 2012 as 16:00
+          }
+        </h6>
+        <h3>{category}</h3>
+        <p>{text}</p>
+        {image && (
+          <Figure>
+            <Figure.Image
+              width={171}
+              height={180}
+              alt="post-image"
+              src={image}
+            />
+          </Figure>
+        )}
+      </li>
+    </Container>
   );
 };
---
+++ social-media-feed-linklei/src/Components/Post/styles.jsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  background-color: #fff;
+  margin: 30px;
+  width: 700px;
+  list-style: none;
+`;
